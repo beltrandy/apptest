ab4edd523fb2f36d307c8dbddfcf339ff6731dc8
Randy Beltran <beltrandy@outlook.com>
2017-07-18 12:05:27 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    message: 'Welcome to the home page of Some Website',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Program Error!"
+  })
+})
+
+app.listen(3000);
